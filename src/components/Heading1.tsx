8d76215a5c189a716d5381fbb837abaeb3aa779c
Louis Gustavo <louis.gustavo@traveloka.com>
2024-12-21 20:37:51 +0700

    Refactor components

--- src/components/Heading1.tsx
+++ src/components/Heading1.tsx
@@ -1,12 +1,10 @@
-import { ReactNode } from 'react';
+import { ReactNode } from 'react'
 
-interface Heading1Props {
-  children: ReactNode;
-  withMarginBottom?: boolean;
+type Heading1Props = {
+  children: ReactNode
+  withMarginBottom?: boolean
 }
 
 export default function Heading1({ children, withMarginBottom = true }: Heading1Props) {
-  return (
-    <h1 className={`text-2xl lg:text-4xl font-bold mb-6 ${withMarginBottom && 'lg:mb-12'}`}>{children}</h1>
-  )
+  return <h1 className={`text-2xl lg:text-4xl font-bold mb-6 ${withMarginBottom && 'lg:mb-12'}`}>{children}</h1>
 }
--- src/components/Top5Container.tsx
+++ src/components/Top5Container.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-interface Top5ContainerProps {
+type Top5Item = {
+  name: string
+  image: string
+  artists: string[]
+  url: string
+}
+type Top5ContainerProps = {
   type: 'artists' | 'tracks'
-  items: []
+  items: Top5Item[]
 }
 
 export default function Top5Container({ type, items }: Top5ContainerProps) {
@@ -12,10 +18,11 @@ export default function Top5Container({ type, items }: Top5ContainerProps) {
       <h2 className='text-lg lg:text-2xl font-medium'>Your Top {type === 'artists' ? 'Artists' : 'Tracks'}</h2>
       <ul>
         {items.map(({ name, image, artists, url }, index: number) => (
-          <li
-            key={index}
-          >
-            <Link href={url} className='flex items-center gap-4 rounded-xl px-2 cursor-pointer text-white no-underline hover:text-lime-500 hover:no-underline md:gap-6 py-3 md:py-4'>
+          <li key={index}>
+            <Link
+              href={url}
+              className='flex items-center gap-4 rounded-xl px-2 cursor-pointer text-white no-underline hover:text-lime-500 hover:no-underline md:gap-6 py-3 md:py-4'
+            >
               <span className='w-1/12 text-2xl lg:text-4xl font-semibold tracking-tighter'>#{index + 1}</span>
               <div className='w-11/12 flex items-center'>
                 <Image
@@ -27,7 +34,7 @@ export default function Top5Container({ type, items }: Top5ContainerProps) {
                 />
                 <div className='flex flex-col'>
                   <span className='font-bold'>{name}</span>
-                  {artists && <span className='text-gray-300'>{artists}</span>}
+                  {artists && <span className='text-gray-300'>{artists.join(', ')}</span>}
                 </div>
               </div>
             </Link>
